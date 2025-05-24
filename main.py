8f2c501b59ef33ba3c99c2b81ff957e8df889b71
jakubpedryc <jakubpedryc@gmail.com>
2025-05-24 14:44:05 +0200

    Cleaning part 2

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from genetic_algorithm.gui import create_gui 
+import genetic_algorithm
+from genetic_algorithm.evaluation import schwefel
 
 if __name__ == "__main__":
-    create_gui()
+    print("brum")
+
+def run_genetic_algorithm(mutation_function, crossover_function, selection_function, mutation_rate, elitism_rate, pop_size, gens, x_min, x_max, dim):
+    best_solution = genetic_algorithm(mutation_function, crossover_function, selection_function, mutation_rate, elitism_rate, pop_size, gens, x_min, x_max, dim)
+    print("Best solution:", best_solution)
+    print("Score:", schwefel(best_solution))
