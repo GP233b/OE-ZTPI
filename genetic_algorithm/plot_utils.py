cef4874c00ab2ac3a729f507bbddf4fefedccd4c
Macieklabuz <101704669+Macieklabuz@users.noreply.github.com>
2025-06-10 20:11:43 +0200

    Update plot_utils.py

---
+++ genetic_algorithm/plot_utils.py
@@ -0,0 +1,39 @@
+# genetic_algorithm/plot_utils.py
+
+import os
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from datetime import datetime
+
+def create_plot_folder(folder='plots'):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    return folder
+
+def plot_fitness_surface(fitness_func, x_min, x_max, resolution=100, folder='plots', fitness_name='fitness'):
+    folder = create_plot_folder(folder)
+
+    x = np.linspace(x_min, x_max, resolution)
+    y = np.linspace(x_min, x_max, resolution)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+
+    for i in range(resolution):
+        for j in range(resolution):
+            point = [X[i, j], Y[i, j]]
+            Z[i, j] = fitness_func(point)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
+    ax.set_title(f'Fitness function surface: {fitness_name}')
+    ax.set_xlabel('X1')
+    ax.set_ylabel('X2')
+    ax.set_zlabel('Fitness')
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"{folder}/{fitness_name}_surface_{timestamp}.png"
+    plt.savefig(filename)
+    plt.close(fig)
+    return filename
