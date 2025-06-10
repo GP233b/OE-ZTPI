58880f0cee404bc6365aa7ecb4a27482911f0657
Macieklabuz <johndoe@example.com>
2025-06-10 18:12:21 +0200

    Added png to folder

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import genetic_algorithm.evaluation as evaluation
 import genetic_algorithm.mutation as mutation
 import genetic_algorithm.crossover as crossover
 import genetic_algorithm.selection as selection
+from genetic_algorithm import plot_utils
 
 app = Flask(__name__)
 CORS(app)
@@ -39,6 +40,12 @@ def api_run_genetic_algorithm():
         if not all([mutation_func, crossover_func, selection_func]):
             return jsonify({'error': 'Invalid function name(s) provided.'}), 400
 
+        # Generowanie wykresu powierzchni funkcji fitness jeśli dim==2
+        if dim == 2:
+            plot_path = plot_utils.plot_fitness_surface(fitness_func, x_min, x_max, resolution=100, fitness_name=fitness_name)
+        else:
+            plot_path = None
+
         best_solution, history, best_individuals, full_data = genetic_algorithm(
             mutation_func,
             crossover_func,
@@ -55,15 +62,17 @@ def api_run_genetic_algorithm():
 
         score = fitness_func(best_solution)
 
-
-        return jsonify({
+        response = {
             'best_solution': best_solution.tolist(),
             'score': score,
             'history': history,
             'best_individuals': best_individuals,
-            'full_data': full_data 
-        })
+            'full_data': full_data
+        }
+        if plot_path:
+            response['fitness_surface_plot'] = plot_path
 
+        return jsonify(response)
 
     except Exception as e:
         app.logger.exception("Algorithm execution error")
