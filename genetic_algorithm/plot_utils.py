dba8b89e4a26b14f5ff52254411c3b22bd983373
Macieklabuz <101704669+Macieklabuz@users.noreply.github.com>
2025-06-10 20:40:54 +0200

    Update plot_utils.py

--- genetic_algorithm/plot_utils.py
+++ genetic_algorithm/plot_utils.py
@@ -1,4 +1,4 @@
-
+# genetic_algorithm/plot_utils.py
 
 import os
 import matplotlib.pyplot as plt
@@ -11,9 +11,10 @@ def create_plot_folder(folder='plots'):
         os.makedirs(folder)
     return folder
 
-def plot_fitness_surface(fitness_func, x_min, x_max, resolution=100, folder='plots', fitness_name='fitness'):
-    folder = create_plot_folder(folder)
-
+def plot_fitness_surface(fitness_func, x_min, x_max, resolution=100,
+                         folder='plots', fitness_name='fitness',
+                         best_solution=None, best_score=None):
+    
     x = np.linspace(x_min, x_max, resolution)
     y = np.linspace(x_min, x_max, resolution)
     X, Y = np.meshgrid(x, y)
@@ -21,19 +22,36 @@ def plot_fitness_surface(fitness_func, x_min, x_max, resolution=100, folder='plo
 
     for i in range(resolution):
         for j in range(resolution):
-            point = [X[i, j], Y[i, j]]
-            Z[i, j] = fitness_func(point)
+            Z[i, j] = fitness_func([X[i, j], Y[i, j]])
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
-    ax.set_title(f'Fitness function surface: {fitness_name}')
+
+    if best_solution is not None and best_score is not None:
+        ax.scatter(
+            best_solution[0],
+            best_solution[1],
+            best_score,
+            color='red',
+            s=50,
+            label='Best solution'
+        )
+        ax.legend()
+
+    title = f'Fitness function surface: {fitness_name}'
+    if best_score is not None:
+        title += f"\nBest score: {best_score:.4f}"
+
+    ax.set_title(title)
     ax.set_xlabel('X1')
     ax.set_ylabel('X2')
     ax.set_zlabel('Fitness')
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    create_plot_folder(folder)
     filename = f"{folder}/{fitness_name}_surface_{timestamp}.png"
     plt.savefig(filename)
     plt.close(fig)
+
     return filename
